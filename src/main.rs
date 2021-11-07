2279b3328d7be3c261f767b8c7de2d03d164b677
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2021-11-07 12:02:47 +0100

    Add winit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::default().build(&event_loop).unwrap();
+    event_loop.run(|event, _target, control_flow| {
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } =>  {
+                *control_flow = ControlFlow::Exit;
+            }
+            Event::MainEventsCleared => {}
+            Event::RedrawRequested(_) => {}
+            _ => {}
+        }
+    });
 }
