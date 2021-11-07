cbd44621ada596d2bbe6ecdb8ed765dcc993b9c3
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2021-11-07 16:01:50 +0100

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use env_logger::Builder;
+use log::LevelFilter;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 
+use log::debug;
 
 fn main() {
+    Builder::new().filter_module(module_path!() , LevelFilter::max()).init();
     let event_loop = EventLoop::new();
     let _window = WindowBuilder::default().build(&event_loop).unwrap();
     event_loop.run(|event, _target, control_flow| {
         match event {
             Event::WindowEvent { event: WindowEvent::CloseRequested, .. } =>  {
+                debug!("Closing app");
                 *control_flow = ControlFlow::Exit;
             }
             Event::MainEventsCleared => {}
