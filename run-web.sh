aa987cfbfa0dc72dffde507409c23f21013199ae
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2021-11-22 22:47:26 +0100

    Renamed project

--- run-web.sh
+++ run-web.sh
@@ -5,6 +5,6 @@ set -xe
 BUILD_DIR=target/wasm32-unknown-unknown/debug
 
 cargo build --target wasm32-unknown-unknown
-wasm-bindgen --target web --out-dir $BUILD_DIR $BUILD_DIR/intro-clock-wgpu.wasm
+wasm-bindgen --target web --out-dir $BUILD_DIR $BUILD_DIR/intro-cube-wgpu.wasm
 cp web/index.html $BUILD_DIR
 python3 -m http.server -d $BUILD_DIR
